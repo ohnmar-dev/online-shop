8eb5968b7619a6ecf7b54ac1ecbf8e839fbae098
ohnmar-dev <ohnmarhtay2021.aa@gmail.com>
2022-06-17 19:52:11 +0630

    created user

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser=require('body-parser')
 const path=require('path')
 const app=express();
 
-// const User=require('./models/user')
+const User=require('./models/user')
 
 app.set('view engine', 'ejs')
 app.set('views','views');
@@ -14,14 +14,14 @@ const adminRouters=require('./routes/admin')
 const shopRouter=require('./routes/shop')
 const errorController=require('./controllers/errorController')
 
-// app.use((req, res, next)=> {
-//     User.findById('62a41c7b02cb100923a7506f')
-//         .then(user => {
-//             req.user =new User(user.name,user.email,user.cart,user._id);
-//             next()
-//         })
-//         .catch(err=>console.log(err))
-// })
+app.use((req, res, next)=> {
+    User.findById('62ac7d612bf3d05201e719a5')
+        .then(user => {
+            req.user =user;
+            next()
+        })
+        .catch(err=>console.log(err))
+})
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/admin',adminRouters)
@@ -38,6 +38,20 @@ mongoogse.connect(
     
 )
 .then(()=>{
+    User.findOne().then(user=>{
+        if(!user){
+            const user=new User({
+                name:'pone pone',
+                email:'pone@gmail.com',
+                cart:{
+                    item:[]
+                }
+            })
+            user.save();
+        }
+    })
+   
+    console.log("Connected !")
     app.listen(3000);
 })
 .catch(err=>console.log(err))
