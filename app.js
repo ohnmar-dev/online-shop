a026b0d2d7d52a378a37d295a472df231f6155f4
ohnmar-dev <ohnmarhtay2021.aa@gmail.com>
2022-06-23 08:16:35 +0630

    usig the session middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser')
 const path=require('path')
 const app=express();
 const session=require('express-session')
+const MongodbStore=require('connect-mongodb-session')(session)
 const User=require('./models/user')
 
 app.set('view engine', 'ejs')
@@ -14,12 +15,19 @@ const adminRouters=require('./routes/admin')
 const shopRouter=require('./routes/shop')
 const authRoutes=require('./routes/auth')
 const errorController=require('./controllers/errorController')
+const MONGO_URI='mongodb+srv://root:root@cluster0.gkh0oe5.mongodb.net/online-shop?retryWrites=true&w=majority'
+//for connect-mongodb-sesssion
+const store=new MongodbStore({
+    uri:MONGO_URI,
+    collection:'session'
+})
 
 //for expression session
 app.use(session({
     secret:'my secret',
     resave:false,
     saveUninitialized:false,
+    newstore:store
     // cookie:{maxAge}
 }))
 
@@ -44,7 +52,7 @@ app.use(errorController.getError)
 
 
 mongoogse.connect(
-    'mongodb+srv://root:root@cluster0.gkh0oe5.mongodb.net/online-shop?retryWrites=true&w=majority',
+    uri,
     {useNewUrlParser:true, useUnifiedTopology:true}
     
 )
