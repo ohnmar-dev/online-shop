3dcb0756c15026d666bd9d4fcb1987cf8e05f64d
ohnmar-dev <ohnmarhtay2021.aa@gmail.com>
2022-07-01 12:48:30 +0630

    adding authentication

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -82,9 +82,7 @@ exports.postEditProduct=(req,res,next)=>{
    
 }
 exports.getProducts=(req,res,next)=>{
-   Product.find()
-//    .select(' price image -_id')
-//    .populate('userId','name')
+   Product.find({userId:req.user._id})
    .then(products=>{ 
     res.render('admin/product', {
         prods:products,
