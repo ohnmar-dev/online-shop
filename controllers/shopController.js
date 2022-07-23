fab89521ec9ff0d19d3e885521b74b50f768b81d
ohnmar-dev <ohnmarhtay2021.aa@gmail.com>
2022-07-23 13:28:59 +0630

    create read Stream

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -204,14 +204,18 @@ exports.getIndex=(req,res,next)=>{
         }
         const invoiceName='invoice-'+orderId+'.pdf'
         const invoicePath=path.join('data','invoice',invoiceName)
-        fs.readFile(invoicePath,(err,data)=>{
-          if(err){
-            return next(err);
-          }
-          res.setHeader('Content-Type','application/pdf')
-          res.setHeader('Content-Disposition',`inline;filename=${invoicePath}`);
-          res.send(data);
-        })
+        // fs.readFile(invoicePath,(err,data)=>{
+        //   if(err){
+        //     return next(err);
+        //   }
+        //   res.setHeader('Content-Type','application/pdf')
+        //   res.setHeader('Content-Disposition',`inline;filename=${invoicePath}`);
+        //   res.send(data);
+        // })
+        const file=fs.createReadStream(invoicePath)
+        res.setHeader('Content-Type','application/pdf')
+        res.setHeader('Content-Disposition',`inline;filename=${invoicePath}`)
+        file.pipe(res)
       })
       .catch(err=>next(err))
   }
